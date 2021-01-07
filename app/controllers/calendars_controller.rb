372c217857f066ffbda57e3b508756fa26f29951
ootakaebisu <ootakaebisu@gmail.com>
2021-01-07 18:15:08 +0900

    [Fix]レイアウト修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,6 @@
 class CalendarsController < ApplicationController
   def index
+    @calendars = Calendar.all
     # ここから/部分テンプレート呼び出し
     if current_user.missions.present? && current_user.missions.find_by(status: "doing").present?
       @mission = Mission.find_by(user_id: current_user.id, status: "doing")
@@ -7,7 +8,6 @@ class CalendarsController < ApplicationController
       @daily_clear_status = @mission.daily_clears.find_by(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
     end
     # ここまで/部分テンプレート呼び出し
-    @calendars = Calendar.all
   end
 
   def create
