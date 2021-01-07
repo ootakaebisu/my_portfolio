0c7e6256d5a7c143cc2922e02572d4dff50e0b53
ootakaebisu <ootakaebisu@gmail.com>
2021-01-07 20:47:44 +0900

    [add]calendarの入力フォーム作成

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,6 @@
 class CalendarsController < ApplicationController
   def index
+    @calendar = Calendar.new
     @calendars = Calendar.all
     # ここから/部分テンプレート呼び出し
     if current_user.missions.present? && current_user.missions.find_by(status: "doing").present?
@@ -11,8 +12,19 @@ class CalendarsController < ApplicationController
   end
 
   def create
+    @calendar = Calendar.new(calendar_params)
+    if @calendar.save
+      redirect_to calendars_path
+    else
+      render :index
+    end
   end
 
   def update
+  end
+
+  protected
+  def calendar_params
+    params.require(:calendar).permit(:user_id, :title, :description, :start_date, :end_date)
   end
 end
